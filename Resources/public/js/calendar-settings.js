1070111afec15625260dfedac3c53760384de02a
Francisco López <30365874+flopezlosada@users.noreply.github.com>
2019-12-11 11:05:26 +0100

    Update calendar-settings.js

--- Resources/public/js/calendar-settings.js
+++ Resources/public/js/calendar-settings.js
@@ -8,25 +8,43 @@ $(function () {
         header: {
             left: 'prev, next',
             center: 'title',
-            right: 'month, basicWeek, basicDay,'
+            right: 'month,basicWeek,basicDay,'
         },
-        lazyFetching: true,
+        weekNumbers:true,
+        firstDay: 1,
+        fixedWeekCount:false,
+        lazyFetching:true,
         timeFormat: {
             // for agendaWeek and agendaDay
-            agenda: 'h:mmt',    // 5:00 - 6:30
+            agenda: 'h:mmt', // 5:00 - 6:30
 
             // for all other views
-            '': 'h:mmt'         // 7p
+            '': 'h:mmt'            // 7p
+        },
+        monthNames:['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        dayNames:['Domingo', 'Lunes', 'Martes', 'Miércoles','Jueves', 'Viernes', 'Sábado'],
+        monthNamesShort:['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun','Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+
+
+        dayNamesShort:['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sáb'],
+        buttonText: {
+            prev:     '&nbsp;&#9668;&nbsp;',  // left triangle
+            next:     '&nbsp;&#9658;&nbsp;',  // right triangle
+            prevYear: '&nbsp;&lt;&lt;&nbsp;', // <<
+            nextYear: '&nbsp;&gt;&gt;&nbsp;', // >>
+            today:    'hoy',
+            month:    'mes',
+            week:     'semana',
+            day:      'día'
         },
         eventSources: [
             {
                 url: Routing.generate('fullcalendar_loader'),
                 type: 'POST',
-                // A way to add custom filters to your event listeners
                 data: {
                 },
                 error: function() {
-                   //alert('There was an error while fetching Google Calendar!');
+                    // alert('There was an error while fetching Google Calendar!');
                 }
             }
         ]
