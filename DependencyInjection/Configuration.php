19181cb398c59b9fc5b078b829e3fab569857596
Paco López <paco.lopez@toq.io>
2026-05-15 15:40:59 +0200

    fix(sf7): añade tipo de retorno TreeBuilder a getConfigTreeBuilder
    ConfigurationInterface en Symfony 7 declara
      getConfigTreeBuilder(): TreeBuilder
    con tipo de retorno obligatorio. Sin él, PHP 8 lanza:
      'Declaration of Configuration::getConfigTreeBuilder() must be
       compatible with ConfigurationInterface::getConfigTreeBuilder():
       Symfony\Component\Config\Definition\Builder\TreeBuilder'
    
    Fix necesario para que el bundle funcione con SF 6.x+ / 7.x.
    
    Hasta ahora el fix solo estaba editado en el vendor/ del consumidor
    (csa-vega), no en el fork. El CI lo destapa porque clona del repo.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('a_designs_calendar');
 
